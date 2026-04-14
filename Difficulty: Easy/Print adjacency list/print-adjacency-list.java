e824f61468831f994bc67e07692285d1b71ba104
Likhith Ponnada <plnlikhith@gmail.com>
2026-04-14 08:53:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Print adjacency list/print-adjacency-list.java
@@ -0,0 +1,17 @@
+// User function Template for Java
+class Solution {
+    public List<List<Integer>> printGraph(int V, int edges[][]) {
+        List <List<Integer>> adj = new ArrayList<>(V);
+        for(int i=0; i<V;i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int[] edge : edges){
+            int u = edge[0];
+            int v = edge[1];
+            adj.get(u).add(v);
+            adj.get(v).add(u);
+        }
+        return adj;
+        
+    }
+}
