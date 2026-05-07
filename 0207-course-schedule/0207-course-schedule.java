0867c1381257a0d1569e8a49e5650262458851c5
Likhith Ponnada <plnlikhith@gmail.com>
2026-05-07 12:44:46 +0530

    Time: 4 ms (93.23%), Space: 47.4 MB (12.51%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.java
@@ -0,0 +1,55 @@
+class Solution {
+
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+
+        List<List<Integer>> graph = new ArrayList<>();
+
+        for (int i = 0; i < numCourses; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int[] p : prerequisites) {
+            graph.get(p[1]).add(p[0]);
+        }
+
+        int[] state = new int[numCourses];
+
+        for (int i = 0; i < numCourses; i++) {
+
+            if (state[i] == 0) {
+
+                if (dfs(i, graph, state)) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public boolean dfs(int node,
+                       List<List<Integer>> graph,
+                       int[] state) {
+
+        if (state[node] == 1) {
+            return true;
+        }
+
+        if (state[node] == 2) {
+            return false;
+        }
+
+        state[node] = 1;
+
+        for (int neighbor : graph.get(node)) {
+
+            if (dfs(neighbor, graph, state)) {
+                return true;
+            }
+        }
+
+        state[node] = 2;
+
+        return false;
+    }
+}
