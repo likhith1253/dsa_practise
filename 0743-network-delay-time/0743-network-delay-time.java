604b5d3fe0a50fd3a3a30189a262aa92db00f5ed
Likhith Ponnada <plnlikhith@gmail.com>
2026-05-21 08:31:09 +0530

    Time: 13 ms (46.52%), Space: 48.4 MB (96.27%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.java
+++ 0743-network-delay-time/0743-network-delay-time.java
@@ -1,50 +1,23 @@
-import java.util.*;
-
 class Solution {
     public int networkDelayTime(int[][] times, int n, int k) {
-        List<List<int[]>> adj = new ArrayList<>();
-        for(int i = 0; i <= n; i++){
-            adj.add(new ArrayList<>());
-        }
-        for(int[] edge : times){
-            adj.get(edge[0]).add(new int[]{edge[1], edge[2]});
-        }
-        
-        int[] distance = new int[n+1];
-        Arrays.fill(distance, Integer.MAX_VALUE);
-        distance[k] = 0;
-        
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[1] - b[1]);
-        pq.offer(new int[]{k, 0});
-
-        while(!pq.isEmpty()){
-            int[] curr = pq.poll();
-            int u = curr[0];
-            int dcurr = curr[1];
-            
-            if(dcurr > distance[u]){
-                continue;
-            }
-            
-            for(int[] arr: adj.get(u)){
-                int v = arr[0];
-                int d = arr[1];
-                
-                if(dcurr + d < distance[v]){
-                    distance[v] = dcurr + d;
-                    pq.offer(new int[]{v, distance[v]});
+        int[] dist = new int[n+1];
+        Arrays.fill(dist,Integer.MAX_VALUE);
+        dist[k] = 0;
+        for(int i =1;i<=n;i++){
+            for(int[] edge : times){
+                int u = edge[0];
+                int v = edge[1];
+                int w = edge[2];
+                if(dist[u] != Integer.MAX_VALUE && dist[u] + w < dist[v]){
+                    dist[v] = dist[u] + w;
                 }
             }
         }
-        
-        int mintime = 0;
-        for(int i = 1; i <= n; i++){
-            if(distance[i] == Integer.MAX_VALUE){
-                return -1;
-            }
-            mintime = Math.max(mintime, distance[i]);
+        int maxDist = 0;
+        for (int i = 1; i <= n; i++) {
+            if (dist[i] == Integer.MAX_VALUE) return -1;
+            maxDist = Math.max(maxDist, dist[i]);
         }
-        
-        return mintime;
+        return maxDist;
     }
 }
