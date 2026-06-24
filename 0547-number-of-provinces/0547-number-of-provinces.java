575d92a4483ee95c3b3233f93cd3285e77af0dbb
Likhith Ponnada <plnlikhith@gmail.com>
2026-06-24 18:41:40 +0530

    Time: 1 ms (86.95%), Space: 47.4 MB (31.19%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,22 +1,30 @@
+
 class Solution {
     public int findCircleNum(int[][] isConnected) {
+        if (isConnected == null || isConnected.length == 0) {
+            return 0;
+        }
+
         int n = isConnected.length;
         boolean[] visited = new boolean[n];
-        int count = 0;
-        for(int i =0; i<isConnected.length;i++){
-            if(!visited[i]){
-                count++;
-                dfsrecursive(isConnected,i,visited);
+        int provinces = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                provinces++;
+                dfs(isConnected, visited, i);
             }
         }
-        return count;
-    }
-    public void dfsrecursive(int[][] isConnected,int node,boolean[] visited){
-        visited[node] = true;
 
-        for(int i =0;i<isConnected.length;i++){
-            if(isConnected[node][i] == 1 && !visited[i]){
-                dfsrecursive(isConnected,i,visited);
+        return provinces;
+    }
+
+    private void dfs(int[][] isConnected, boolean[] visited, int city) {
+        visited[city] = true;
+
+        for (int neighbor = 0; neighbor < isConnected.length; neighbor++) {
+            if (isConnected[city][neighbor] == 1 && !visited[neighbor]) {
+                dfs(isConnected, visited, neighbor);
             }
         }
     }
