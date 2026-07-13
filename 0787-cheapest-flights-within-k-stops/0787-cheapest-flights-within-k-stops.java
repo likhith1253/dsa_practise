863bf3837c840ab68997359b80372309e6ce01ce
Likhith Ponnada <plnlikhith@gmail.com>
2026-07-13 17:06:06 +0530

    Time: 6 ms (85.71%), Space: 46.5 MB (70.77%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -1,44 +1,61 @@
 import java.util.*;
 
 class Solution {
+
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        List<List<int[]>> adj = new ArrayList<>();
+
+        // Build adjacency list
+        List<List<int[]>> graph = new ArrayList<>();
+
         for (int i = 0; i < n; i++) {
-            adj.add(new ArrayList<>());
-        }
-        for (int[] edge : flights) {
-            adj.get(edge[0]).add(new int[]{edge[1], edge[2]});
+            graph.add(new ArrayList<>());
         }
 
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{src, 0}); 
+        for (int[] flight : flights) {
+            graph.get(flight[0]).add(new int[]{flight[1], flight[2]});
+        }
 
-        int[] minCost = new int[n];
-        Arrays.fill(minCost, Integer.MAX_VALUE);
-        minCost[src] = 0;
+        // Minimum cost to reach each city
+        int[] cost = new int[n];
+        Arrays.fill(cost, Integer.MAX_VALUE);
+        cost[src] = 0;
+
+        // {city, totalCost}
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{src, 0});
 
         int stops = 0;
 
-        while (!q.isEmpty() && stops <= k) {
-            int size = q.size();
+        while (!queue.isEmpty() && stops <= k) {
+
+            int size = queue.size();
+
+            // Process one level (one extra flight)
             while (size-- > 0) {
-                int[] curr = q.poll();
-                int node = curr[0];
-                int cost = curr[1];
 
-                for (int[] neighbor : adj.get(node)) {
-                    int nextNode = neighbor[0];
-                    int edgeCost = neighbor[1];
+                int[] curr = queue.poll();
 
-                    if (cost + edgeCost < minCost[nextNode]) {
-                        minCost[nextNode] = cost + edgeCost;
-                        q.add(new int[]{nextNode, cost + edgeCost});
+                int city = curr[0];
+                int currCost = curr[1];
+
+                for (int[] neighbour : graph.get(city)) {
+
+                    int nextCity = neighbour[0];
+                    int price = neighbour[1];
+
+                    int newCost = currCost + price;
+
+                    if (newCost < cost[nextCity]) {
+
+                        cost[nextCity] = newCost;
+                        queue.offer(new int[]{nextCity, newCost});
                     }
                 }
             }
+
             stops++;
         }
 
-        return minCost[dst] == Integer.MAX_VALUE ? -1 : minCost[dst];
+        return cost[dst] == Integer.MAX_VALUE ? -1 : cost[dst];
     }
 }
