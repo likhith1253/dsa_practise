04f8aec82486914a87fe65c06eef6ac701a908e7
Likhith Ponnada <plnlikhith@gmail.com>
2026-07-13 09:11:13 +0530

    Time: 42 ms (55.03%), Space: 48 MB (40.94%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
@@ -1,39 +1,69 @@
-import java.util.*;
-
 class Solution {
+
     public int minimumEffortPath(int[][] heights) {
+
         int m = heights.length;
         int n = heights[0].length;
-        int[][] effort = new int[m][n];
-        for (int[] row : effort) {
-            Arrays.fill(row, Integer.MAX_VALUE);
+
+        int[][] dist = new int[m][n];
+
+        for(int i = 0; i < m; i++) {
+            Arrays.fill(dist[i], Integer.MAX_VALUE);
         }
-        effort[0][0] = 0;
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[2] - b[2]);
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>(
+                (a, b) -> Integer.compare(a[2], b[2])
+            );
+
+        int[][] dir = {
+            {1, 0},
+            {-1, 0},
+            {0, 1},
+            {0, -1}
+        };
+
+        dist[0][0] = 0;
+
         pq.offer(new int[]{0, 0, 0});
-        int[][] dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-        
-        while (!pq.isEmpty()) {
+
+        while(!pq.isEmpty()) {
+
             int[] curr = pq.poll();
+
             int r = curr[0];
             int c = curr[1];
-            int e = curr[2];
-            
-            if (r == m - 1 && c == n - 1) return e;
-            if (e > effort[r][c]) continue;
-            
-            for (int[] dir : dirs) {
-                int nr = r + dir[0];
-                int nc = c + dir[1];
-                if (nr >= 0 && nr < m && nc >= 0 && nc < n) {
-                    int nextEffort = Math.max(e, Math.abs(heights[r][c] - heights[nr][nc]));
-                    if (nextEffort < effort[nr][nc]) {
-                        effort[nr][nc] = nextEffort;
-                        pq.offer(new int[]{nr, nc, nextEffort});
+            int effort = curr[2];
+
+            if(r == m - 1 && c == n - 1) {
+                return effort;
+            }
+
+            if(effort > dist[r][c]) {
+                continue;
+            }
+
+            for(int[] d : dir) {
+
+                int nr = r + d[0];
+                int nc = c + d[1];
+
+
+                if(nr >= 0 && nc >= 0 && nr < m && nc < n) {
+
+                    int newEffort = Math.max(
+                        effort,
+                        Math.abs(heights[r][c] - heights[nr][nc])
+                    );
+
+                    if(newEffort < dist[nr][nc]) {
+                        dist[nr][nc] = newEffort;
+
+                        pq.offer(new int[]{nr, nc, newEffort});
                     }
                 }
             }
         }
+
         return 0;
     }
 }
